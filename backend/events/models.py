c71319a20d6be3ed2a8299d9afe3f0f308b01896
harshraj2717 <harshraj2717@gmail.com>
2024-06-01 03:12:37 +0530

    use proper blank and null constrains in models

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 
 
 # Create your models here.
 class Event(models.Model):
     # Keeping `id` as the primary key instead of name for easier querying via views/serializers
     name = models.CharField(max_length=255, unique=True, blank=False)
-    cover_image = models.ImageField(upload_to="event_images/", blank=True, null=True)
+    cover_image = models.ImageField(upload_to="event_images/", blank=True)
     description = models.TextField(blank=False)
     venue = models.TextField(blank=False, default="TBA")
     city = models.CharField(max_length=255, blank=False, default="TBA")
-    venue_map_link = models.URLField(blank=True, null=True)
-    date_time = models.DateTimeField(blank=False)
+    venue_map_link = models.URLField(blank=True)
+    date_time = models.DateTimeField(blank=False, null=False)
 
     def __str__(self) -> str:
         return f"{self.name} @ {self.city} ({self.date_time.date()})"
@@ -30,18 +29,28 @@ class EventRegistration(models.Model):
     ]
 
     event = models.ForeignKey(
-        Event, on_delete=models.CASCADE, related_name="registrations"
+        Event,
+        on_delete=models.CASCADE,
+        related_name="registrations",
+        blank=False,
+        null=False,
     )
-    email = models.EmailField()
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    occupation = models.CharField(max_length=20, choices=OCCUPATION_CHOICES)
-    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
-    linkedin = models.URLField(blank=False, null=False)
-    github = models.URLField(blank=True, null=True)
-    twitter = models.URLField(blank=True, null=True)
-    other_links = models.URLField(blank=True, null=True)
-    # TODO imnplement this (RSVP mailing + RSVP submission link)
+    email = models.EmailField(blank=False, null=False)
+    first_name = models.CharField(max_length=255, blank=False)
+    last_name = models.CharField(max_length=255, blank=False)
+    occupation = models.CharField(
+        max_length=20, choices=OCCUPATION_CHOICES, blank=False
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICES,
+        blank=False,
+    )
+    linkedin = models.URLField(blank=False)
+    github = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    other_links = models.URLField(blank=True)
+    # TODO: imnplement this (RSVP mailing + RSVP submission link)
     rsvp = models.BooleanField(default=False, blank=False)
 
     class Meta:
