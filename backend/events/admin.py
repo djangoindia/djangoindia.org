565cf5b5aede796d9e61d89075ffd3e45f425f39
harshraj2717 <harshraj2717@gmail.com>
2024-06-01 14:27:52 +0530

    add event registration inline

--- backend/events/admin.py
+++ backend/events/admin.py
@@ -14,9 +14,16 @@ class EventForm(forms.ModelForm):
         }
 
 
+class EventRegistrationInline(admin.TabularInline):
+    model = EventRegistration
+    extra = 0
+    readonly_fields = [field.name for field in EventRegistration._meta.get_fields()]
+
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     form = EventForm
+    inlines = [EventRegistrationInline]
 
 
 admin.site.register(EventRegistration)
