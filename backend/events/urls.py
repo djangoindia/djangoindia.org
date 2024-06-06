acdefe1eb6c3254a196e90e372cbfd677297207b
harshraj2717 <harshraj2717@gmail.com>
2024-06-07 02:03:25 +0530

    move function based views to class based

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 # URL conf
 urlpatterns = [
-    path("", views.get_events, name="get_events"),
-    path("register/", views.register_event, name="register_event"),
+    path("", views.GetEvents.as_view(), name="get_events"),
+    path("register/", views.RegisterEvent.as_view(), name="register_event"),
 ]
--- backend/events/views.py
+++ backend/events/views.py
@@ -1,23 +1,15 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Event
-from .serializers import EventSerializer, EventRegistrationSerializer
-
+from .serializers import EventRegistrationSerializer, EventSerializer
 
 # Create your views here.
-@api_view(["GET"])
-def get_events(request):
-    events = Event.objects.all()
-    serializer = EventSerializer(events, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-@api_view(["POST"])
-def register_event(request):
-    serializer = EventRegistrationSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class GetEvents(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class RegisterEvent(generics.CreateAPIView):
+    serializer_class = EventRegistrationSerializer
