2e8ee702b892abec2ce53c029033814bf487bcd1
DevilsAutumn <bhuvnesh875@gmail.com>
2024-06-21 22:52:56 +0530

    event handling related fix

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 # URL conf
 urlpatterns = [
-    path("", views.EventAPIView.as_view(), name="get_events"),
+    path("", views.EventAPIView.as_view(), name="list_events"),
+    path("<uuid:pk>/", views.EventAPIView.as_view(), name="get_event"),
 ]
