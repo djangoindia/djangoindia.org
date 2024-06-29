eb96460570815c43bc79a69425504ccf6b6be932
Gautam Kumar <0101gautamkumar@gmail.com>
2024-06-30 01:28:56 +0530

    Frontend: All sections and components are completed(#17)
    * added events section events page and join comminity
    
    * completed contact us form and support us section
    
    * correct codemod
    
    * footer completed, navbar and hero section responsiveness is pending
    
    ---------
    
    Co-authored-by: Gautam <0101gautakumar@gmail.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,20 @@
-import HeroSection from "./components/HeroSection/page";
-import Navbar from "./components/Navbar/page";
-
+import Navbar from "@/components/Navbar/page";
+import HeroSection from "@/components/HeroSection/page";
+import Join from "@/components/JoinCommunity/Join";
+import Main from "@/components/EventSection/Main";
+import Update from "@/components/Latestupdate/Update";
+import Support from "@/components/Support/Support";
+import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <HeroSection/>
+      {/* <Navbar/>
+      <HeroSection/> */}
+      <Main />
+      <Join />
+      <Update />
+      <Support />
+      <Footer />
     </>
   );
 }
