cdc38f5fb93386756b6e188c03f7253723e36e57
DevilsAutumn <bhuvnesh875@gmail.com>
2024-07-05 00:39:35 +0530

    minor fixes

--- backend/events/utils.py
+++ backend/events/utils.py
@@ -2,31 +2,28 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
-from .models import Event, EventRegistration
+from .models import EventRegistration
 
 def send_registration_confirmation_email(email, event_id):
     try:
-        # Get the event details
-        event = Event.objects.get(pk=event_id)
-        registration = EventRegistration.objects.get(email=email, event=event)
+        registration = EventRegistration.objects.get(email=email, event_id=event_id)
 
-        # Render the HTML template
         context = {
             'first_name': registration.first_name,
             'event': {
-                'name': event.name,
-                'date_time': event.date_time,
-                'venue': event.venue,
-                'description': event.description,
-                'cover_image': event.cover_image.url if event.cover_image else None,
-                'venue_map_link': event.venue_map_link
+                'name': registration.event.name,
+                'date_time': registration.event.date_time,
+                'venue': registration.event.venue,
+                'description': registration.event.description,
+                'cover_image': registration.event.cover_image if registration.event.cover_image else None,
+                'venue_map_link': registration.event.venue_map_link
             }
         }
         html_content = render_to_string('admin/registration_success.html', context)
-        text_content = strip_tags(html_content)  # Strip the HTML tags for a plain text alternative
+        # Strip the HTML tags for a plain text alternative
+        text_content = strip_tags(html_content)
 
-        # Create and send the email using EmailMultiAlternatives
-        subject = 'Event Registration Successful'
+        subject = f'{registration.event.name} - Event Registration Successful!'
         from_email = settings.DEFAULT_FROM_EMAIL
         to_email = [email]
 
