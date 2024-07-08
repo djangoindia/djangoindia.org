cc952edc43687c9dacc562fe1ea46cbc3170bb46
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-07-08 18:08:18 +0530

    Added Underline active links in navbar

--- frontend/src/components/Navbar/page.tsx
+++ frontend/src/components/Navbar/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import SignButton from "../ui/SignButton";
 import Image from "next/image";
 import logo from "/whatIsDjango/Logo.svg";
 const Navbar = () => {
+const pathname = usePathname();    //current pathname
     return <>
     <section className="h-full w-full relative bg-[#C1CAD2]">
         <div className="h-20 w-full flex justify-around items-center">
@@ -21,15 +26,15 @@ const Navbar = () => {
 
                 {/* menu */}
                 <div className="flex font-semibold gap-16 text-black justify-center items-center">
-                    <div>
-                        <a href="/">Home</a>
-                    </div>
-                    <div>
-                        <a href="#">Events</a>
-                    </div>
-                    <div>
-                        <a href="/Contactus">Contact Us</a>
-                    </div>
+                    <Link href="/" className={`py-5 ${pathname === '/' ? 'border-b-2 border-black' : 'hover:border-b-2 hover:border-black'}`}>
+                        Home
+                    </Link>
+                    <Link href="/" className={`py-5 ${pathname === '#' ? 'border-b-2 border-black' : 'hover:border-b-2 hover:border-black'}`}>
+                        Events
+                    </Link>
+                    <Link href="/Contactus" className={`py-5 ${pathname === '/Contactus' ? 'border-b-2 border-black' : 'hover:border-b-2 hover:border-black'}`}>
+                        Contact Us
+                    </Link>
                 </div>
 
                 {/* button */}
