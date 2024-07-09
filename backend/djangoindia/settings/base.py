8ad7322704c89d65cbbfa53bffb093c96aee81cc
DevilsAutumn <bhuvnesh875@gmail.com>
2024-07-09 07:17:58 +0530

    changed default db to postgres

--- backend/djangoindia/settings/base.py
+++ backend/djangoindia/settings/base.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = "djangoindia.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
 
