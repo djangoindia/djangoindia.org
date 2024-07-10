c589440cebeafa7b5aa0bca29c5f319849092a34
Bhuvnesh <bhuvnesh875@gmail.com>
2024-07-10 18:59:08 +0530

    Backend refactored and default db changes to postgres. (#37)
    * refactored backend
    
    * changed default db to postgres

---
+++ backend/djangoindia/bg_tasks/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BgTasksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djangoindia.bg_tasks'
--- backend/djangoindia/settings/base.py
+++ backend/djangoindia/settings/base.py
@@ -48,10 +48,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "celery",
-    "rest_framework",
-    "storages",
-    "events",
     "corsheaders",
+    "djangoindia.api",
+    "djangoindia.bg_tasks",
+    "djangoindia.db",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -92,8 +93,12 @@ WSGI_APPLICATION = "djangoindia.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
 
