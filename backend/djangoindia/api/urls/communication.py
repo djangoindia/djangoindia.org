33c1f4ff48b1a78bd99346bfdf1b104eee65f89c
Anurag Yadav <yadavanurag1310@gmail.com>
2024-07-16 21:11:54 +0530

    Created API for contact us form

--- backend/djangoindia/api/urls/communication.py
+++ backend/djangoindia/api/urls/communication.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from djangoindia.api.views.communication import NewsletterSubscriptionAPIView
+from djangoindia.api.views.communication import NewsletterSubscriptionAPIView, ContactUsAPIView
 
 urlpatterns = [
     path('newsletter/', NewsletterSubscriptionAPIView.as_view(), name='newsletter-subscription'),
+    path('contact-us/', ContactUsAPIView.as_view(), name='contact-us'),
 ]
