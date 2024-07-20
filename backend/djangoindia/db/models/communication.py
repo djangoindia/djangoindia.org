05ce17ecf61912a005921a30d4f50d5b437b48b4
Anurag Yadav <yadavanurag1310@gmail.com>
2024-07-21 00:51:34 +0530

    update model field type

--- backend/djangoindia/db/models/communication.py
+++ backend/djangoindia/db/models/communication.py
@@ -12,7 +12,7 @@ class ContactUs(BaseModel):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    message = models.CharField(max_length=500)
+    message = models.TextField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
