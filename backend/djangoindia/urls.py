92dc85f52ebc817888daca16372becee0cd7f561
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-08-01 00:25:07 +0530

    Added drf-spectacular for the api documentation (#53)
    * implemented drf-spectacular for the api documentation
    
    * made some required changes in settings and views

--- backend/djangoindia/urls.py
+++ backend/djangoindia/urls.py
@@ -19,10 +19,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path,re_path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     path("api/v1/", include("djangoindia.api.urls")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/v1/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
