a12ec1fa034d81d7495c8bbc422a0c91e02f15a7
Anurag Yadav <95437051+Yadavanurag13@users.noreply.github.com>
2024-08-12 01:25:20 +0530

    Setup nginx for prod deployment using docker (#58)
    * Setup nginx for prod deployment using docker
    
    * made required changes
    
    * improvements in nginx setup
    
    ---------
    
    Co-authored-by: devilsautumn <bhuvnesh875@gmail.com>

--- backend/djangoindia/settings/base.py
+++ backend/djangoindia/settings/base.py
@@ -146,11 +146,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "app/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    '/app/static',
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
