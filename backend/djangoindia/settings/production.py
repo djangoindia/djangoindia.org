ef9ad27b3e45c589e517edd858afb4b7b90aeeba
devilsautumn <bhuvnesh875@gmail.com>
2024-08-15 14:09:43 +0530

    changed static files config

--- backend/djangoindia/settings/production.py
+++ backend/djangoindia/settings/production.py
@@ -15,10 +15,8 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 ADMIN_URL = os.environ.get("DJANGO_ADMIN_URL")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # AWS settings
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
@@ -28,17 +26,11 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # Media files (Images, etc.)
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
-# STORAGES = {
-#     "default": {
-#         "BACKEND": "storages.backends.s3.S3Storage",
-#         "OPTIONS": {
-#             "AWS_ACCESS_KEY_ID": AWS_ACCESS_KEY_ID,
-#             "AWS_SECRET_ACCESS_KEY": AWS_SECRET_ACCESS_KEY,
-#             "AWS_STORAGE_BUCKET_NAME": AWS_STORAGE_BUCKET_NAME,
-#             "AWS_S3_REGION_NAME": AWS_S3_REGION_NAME,
-#         },
-#     },
-#     "staticfiles": {
-#         "BACKEND": "django.core.files.storage.FileSystemStorage",
-#     },
-# }
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+}
