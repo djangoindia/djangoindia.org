9c04dc58014e52ac2e60c8180f5c38f6d9170784
devilsautumn <bhuvnesh875@gmail.com>
2024-08-24 00:01:36 +0530

    seo optimizations

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,7 +6,34 @@ const inter = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Django India',
-  description: 'Welcome django india',
+  description: 'Join the Django India Community to connect with fellow developers, learn best practices, and grow your skills in Django, Python, and web development.',
+  keywords: 'Django, India, Python, Web Development, Community, Programming, Framework, Open Source,',
+  authors: [{ name: "Django India Community", url: "https://djangoindia.org" }],
+  openGraph: {
+    title: 'Django India Community',
+    description: 'Connect with Django developers across India.',
+    url: 'https://djangoindia.org',
+    siteName: 'Django India',
+    images: [
+      {
+        url: '/djangoindia_logo.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_IN',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Django India Community',
+    description: 'Connect with Django developers across India.',
+    images: ['/djangoindia_twitter.png'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 export default function RootLayout({
