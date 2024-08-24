d0c935788b089f1a025ae0385d0d3575ee0a2aa4
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-24 14:34:07 +0530

    add : EventVolunteers Model

---
+++ backend/djangoindia/db/models/volunteers.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .base import BaseModel
+from .event import Event
+
+class EventVolunteers(BaseModel):
+    events = models.ManyToManyField(Event,related_name="events")
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    profile_pic = models.ImageField(upload_to='') # Don't know the location so kept blank
+    linkedin_url = models.URLField()
+    twitter_url = models.URLField()
+    bio = models.CharField()
+
+    def __str__(self):
+        return f"{self.full_name} {self.events.name}"
