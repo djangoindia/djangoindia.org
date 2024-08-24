9cb7986987f0068ce9c9559e4f24cff2a410a54b
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-24 14:41:11 +0530

    add : serializers for EventVolunteer model

---
+++ backend/djangoindia/api/serializers/volunteer.py
@@ -0,0 +1,13 @@
+from db.models.volunteers import EventVolunteers
+from rest_framework import serializers
+
+class EventVolunteerSerializer(serializers.Serializer):
+    full_name = serializers.CharField(max_length=100)
+    email = serializers.EmailField()
+    profile_pic = serializers.ImageField(upload_to='') # Don't know the location so kept blank
+    linkedin_url = serializers.URLField()
+    twitter_url = serializers.URLField()
+    bio = serializers.CharField()
+    
+    def create(self, validated_data):
+        return EventVolunteers.objects.create(**validated_data)
