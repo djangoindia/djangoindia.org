289441cdd29bb3255fbfbab580330ab73060778b
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-28 11:50:34 +0530

    rm : upload_to widget from profile-pic (EventVolunteerSerializer)

--- backend/djangoindia/api/serializers/volunteer.py
+++ backend/djangoindia/api/serializers/volunteer.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class EventVolunteerSerializer(serializers.Serializer):
     full_name = serializers.CharField(max_length=100)
     email = serializers.EmailField()
-    profile_pic = serializers.ImageField(upload_to='') # Don't know the location so kept blank
+    profile_pic = serializers.ImageField()
     linkedin_url = serializers.URLField()
     twitter_url = serializers.URLField()
     bio = serializers.CharField()
