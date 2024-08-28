39caf6f6610c4b1c91e72f694f53d233e9d7ea0e
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-28 21:04:55 +0530

    add : Sponser Model

--- backend/djangoindia/db/models/event.py
+++ backend/djangoindia/db/models/event.py
@@ -89,3 +89,65 @@ class EventRegistration(BaseModel):
         return (
             f"{self.first_name} {self.last_name} ({self.email}) --- {self.event.name}"
         )
+
+    
+
+class Sponsor(BaseModel):
+    
+    INDIVIDUAL = "individual"
+    ORGANIZATION = "organization"
+    
+    SPONSOR_TYPE_CHOICES = [
+        (INDIVIDUAL, INDIVIDUAL),
+        (ORGANIZATION, ORGANIZATION),
+    ]
+
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=SPONSOR_TYPE_CHOICES)
+    logo = models.ImageField(upload_to='sponsors/logos/')
+    url = models.URLField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    
+class Sponsorship(BaseModel):
+    
+    PLATINUM = "platinum"
+    GOLD = "gold"
+    SILVER = "silver"
+    INDIVIDUAL = "individual"
+    ORGANIZATION = "organization"
+
+    COMMUNITY_SPONSORSHIP = "community_sponsorship"
+    EVENT_SPONSORSHIP = "event_sponsorship"
+    
+    SPONSORSHIP_TIER_CHOICES = [
+        (PLATINUM, PLATINUM),
+        (GOLD, GOLD),
+        (SILVER, SILVER),
+        (INDIVIDUAL, INDIVIDUAL),
+        (ORGANIZATION, ORGANIZATION),
+    ]
+
+    SPONSORSHIP_TYPE_CHOICES = [
+        (COMMUNITY_SPONSORSHIP, COMMUNITY_SPONSORSHIP),
+        (EVENT_SPONSORSHIP, EVENT_SPONSORSHIP),
+    ]
+
+    sponsor_details = models.ForeignKey(Sponsor, on_delete=models.CASCADE, related_name='sponsorships')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='sponsors', null=True, blank=True)
+    tier = models.CharField(max_length=20, choices=SPONSORSHIP_TIER_CHOICES)
+    type = models.CharField(max_length=30, choices=SPONSORSHIP_TYPE_CHOICES)
+
+    def clean(self):
+        super().clean()
+        if self.type == self.COMMUNITY_SPONSORSHIP and self.tier not in [self.INDIVIDUAL, self.ORGANIZATION]:
+            raise ValidationError("For community sponsorship, tier must be either 'individual' or 'organization'.")
+        elif self.type == self.EVENT_SPONSORSHIP and self.tier not in [self.PLATINUM, self.GOLD, self.SILVER]:
+            raise ValidationError("For event sponsorship, tier must be either 'platinum', 'gold', or 'silver'.")
+
+
+    def __str__(self):
+        return f"{self.sponsor_details.name} - {self.tier} - {self.type}"
