86262755d4d4b2cde58b677df23331084e690541
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-08-30 00:10:18 +0530

    add : Modelform for Volunteers Registration

--- backend/djangoindia/db/forms.py
+++ backend/djangoindia/db/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from djangoindia.db.models.event import Event
+from djangoindia.db.models.volunteers import EventVolunteers
 
 
 class EventForm(forms.ModelForm):
@@ -10,6 +11,11 @@ class EventForm(forms.ModelForm):
         widgets = {
             "description": forms.Textarea(attrs={"placeholder": "Enter markdown here"})
         }
+
+class EventVolunteerCreationForm(forms.ModelForm):
+    class Meta:
+        model = EventVolunteers
+        fields = ("full_name" , "email" , "profile_pic" , "linkedin_url" , "twitter_url" , "bio",)
 
 class EmailForm(forms.Form):
     subject = forms.CharField(max_length=255, required=True)
