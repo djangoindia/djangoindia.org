73dbeb6f74c38c64de8b9d6cd49e99d20f0fb635
devilsautumn <bhuvnesh875@gmail.com>
2024-09-01 19:01:48 +0530

    minor

--- backend/djangoindia/api/urls/communication.py
+++ backend/djangoindia/api/urls/communication.py
@@ -3,6 +3,6 @@ from django.urls import path
 from djangoindia.api.views.communication import SubscriberAPIView, ContactUsAPIView
 
 urlpatterns = [
-    path('newsletter/', SubscriberAPIView.as_view(), name='newsletter-Subscriber'),
+    path('subscriber/', SubscriberAPIView.as_view(), name='subscriber'),
     path('contact-us/', ContactUsAPIView.as_view(), name='contact-us'),
 ]
