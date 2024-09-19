dd2b361d8a0bf572fd04aa8c10774a82f65956ba
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2024-09-20 01:50:10 +0530

    subscribe added and working

---
+++ frontend/src/components/Modal/modal.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { MdOutlineEmail } from 'react-icons/md';
+import { FaRegUser } from 'react-icons/fa';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubscriberForm } from './LatestUpdate.types';
+import { fetchData } from '@utils';
+import { API_ENDPOINTS, SUBSCRIBER_FORM_SCHEMA } from '@constants';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { enqueueSnackbar } from 'notistack';
+
+interface ModalProps {
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ onClose }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<SubscriberForm>({
+    resolver: yupResolver(SUBSCRIBER_FORM_SCHEMA),
+  });
+
+  const onSubmit: SubmitHandler<SubscriberForm> = async (data) => {
+    const res = await fetchData<{ message: string }>(API_ENDPOINTS.subscriber, {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+    if (res.statusCode === 200 || res.statusCode === 201) {
+      enqueueSnackbar(res?.data?.message, { variant: 'success' });
+    } else if (res.statusCode === 429) {
+      enqueueSnackbar('Too many requests, Please try again after some time.', { variant: 'error' });
+    } else {
+      enqueueSnackbar(res?.error?.message, { variant: 'error' });
+    }
+    reset();
+    onClose(); // Close the modal after form submission
+  };
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="relative w-full max-w-lg p-8 bg-[#f9f4ee] rounded-lg shadow-lg">
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-black text-xl"
+        >
+          &times;
+        </button>
+
+        <h2 className="text-center text-2xl font-semibold text-black mb-6">
+          Subscribe to Django India and never miss an update!
+        </h2>
+
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+          {/* Full Name Field */}
+          <div className="relative">
+            <FaRegUser className="absolute left-3 top-3 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Full Name"
+              className={`w-full pl-10 pr-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-black ${
+                errors.name ? 'border-red-500' : ''
+              }`}
+              {...register('name', { required: true })}
+            />
+            <p className="text-red-500 text-sm">
+              {errors.name?.message ?? ' '}
+            </p>
+          </div>
+
+          {/* Email Field */}
+          <div className="relative">
+            <MdOutlineEmail className="absolute left-3 top-3 text-gray-500" />
+            <input
+              type="email"
+              placeholder="Email"
+              className={`w-full pl-10 pr-4 py-3 border rounded-full focus:outline-none focus:ring-2 focus:ring-black ${
+                errors.email ? 'border-red-500' : ''
+              }`}
+              {...register('email', { required: true })}
+            />
+            <p className="text-red-500 text-sm">
+              {errors.email?.message ?? ' '}
+            </p>
+          </div>
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="bg-black text-white py-3 px-4 rounded-full hover:bg-gray-900 transition-all"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
