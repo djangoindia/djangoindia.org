440b7259c01d6e045f4644990ca07f08d5b686a3
Nitin Gupta <nitin@rocketium.com>
2024-09-23 16:10:40 +0530

    chore: added error handling in events page

--- frontend/src/app/(core)/events/page.tsx
+++ frontend/src/app/(core)/events/page.tsx
@@ -4,17 +4,20 @@ import { EventCard } from '@sections'
 import { fetchData } from '@/utils'
 import { EventsResponse } from '@/types'
 import { API_ENDPOINTS } from '@/constants'
+import { ClientError } from '@/components'
 
 const Page = async () => {
-  const { data: events } = await fetchData<EventsResponse>(
+  const { data: events, error } = await fetchData<EventsResponse>(
     API_ENDPOINTS.events,
   )
+
   return (
     <div>
       <div className='p-4 mb-10 md:mb-20 lg:mb-50'>
         <div className='flex flex-col items-center mb-4'>
           <h1 className='text-3xl text-center font-bold'>All Events</h1>
         </div>
+        {error && error.message && <ClientError error={error} />}
         {events?.length ? (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
             {events?.map(
