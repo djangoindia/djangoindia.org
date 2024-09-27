99f61a86e6ce4f2ce4a062561dad068e44ca35c7
Nitin Gupta <nitin@rocketium.com>
2024-09-27 22:07:14 +0530

    chore: updated some files as per the new config

--- frontend/src/components/ClientError/ClientError.tsx
+++ frontend/src/components/ClientError/ClientError.tsx
@@ -1,22 +1,25 @@
 'use client'
 
-import React, { useEffect } from 'react';
-import { enqueueSnackbar } from 'notistack';
-import { ErrorResponse } from '@/utils';
+import React, { useEffect } from 'react'
+import { enqueueSnackbar } from 'notistack'
+import { ErrorResponse } from '@/utils'
 
 interface ClientComponentProps {
-    error: ErrorResponse;
+  error: ErrorResponse
 }
 
 const ClientComponent: React.FC<ClientComponentProps> = ({ error }) => {
-    
-    useEffect(() => {
-        if (error) {
-            enqueueSnackbar(error?.message, { key: error?.statusCode, variant: 'error', preventDuplicate: true });
-        }
-    }, [error]);
+  useEffect(() => {
+    if (error) {
+      enqueueSnackbar(error?.message, {
+        key: error?.statusCode,
+        variant: 'error',
+        preventDuplicate: true,
+      })
+    }
+  }, [error])
 
-    return (<></>);
-};
+  return <></>
+}
 
-export default ClientComponent;
+export default ClientComponent
