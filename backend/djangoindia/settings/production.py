d0574c61a3c8ccdaaa65a3d139bc6f5f54276199
Bhuvnesh Sharma <bhuvneshsharma@Bhuvneshs-MacBook-Air.local>
2024-10-06 01:14:36 +0530

    Fixed settings

--- backend/djangoindia/settings/production.py
+++ backend/djangoindia/settings/production.py
@@ -14,7 +14,6 @@ EMAIL_USE_TLS = True  # TODO: Set this to False if using SSL
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 ADMIN_URL = os.environ.get("DJANGO_ADMIN_URL")
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # AWS settings
 
