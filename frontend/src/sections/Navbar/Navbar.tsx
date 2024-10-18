4b74e70673d55ff081d7c9320267354bb5710c09
Nitin Gupta <nitin@rocketium.com>
2024-10-18 18:30:11 +0530

    fix: fixed navbar for medium sizes

--- frontend/src/sections/Navbar/Navbar.tsx
+++ frontend/src/sections/Navbar/Navbar.tsx
@@ -122,7 +122,7 @@ const Navbar = () => {
       <section className='h-full w-full relative bg-[#C1CAD2]'>
         <div className='h-12 md:h-20 w-full flex justify-around items-center'>
           {/* logo  */}
-          <div className='hidden md:flex items-center justify-center '>
+          <div className='hidden items-center justify-center'>
             <Image
               src='/whatIsDjango/Logo.svg'
               width={121}
@@ -137,7 +137,7 @@ const Navbar = () => {
           </div>
 
           {/* menu */}
-          {width > 786 ? (
+          {width > 750 ? (
             <>
               <div className='flex font-semibold gap-5 md:gap-16 text-xs md:text-base text-black justify-center items-center'>
                 <Link
@@ -174,7 +174,7 @@ const Navbar = () => {
               <SupportUsDialog />
             </>
           ) : (
-            <button onClick={toggleDrawer} className='md:hidden mr-auto ml-5'>
+            <button onClick={toggleDrawer} className='mr-auto ml-5'>
               {/* Hamburger icon */}
               <HamburgerMenuIcon />
             </button>
