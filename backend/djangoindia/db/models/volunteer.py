f1314a4e1365b76e1c3680f1ca6082fbda7393e8
DevilsAutumn <bhuvnesh875@gmail.com>
2024-10-29 13:13:09 +0530

    added m2m field volunteers in events model.

--- backend/djangoindia/db/models/volunteer.py
+++ backend/djangoindia/db/models/volunteer.py
@@ -1,11 +1,10 @@
 from django.db import models
+
 from .base import BaseModel
-from .event import Event
 
 
 class Volunteer(BaseModel):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE,related_name="volunteers")
-    photo = models.ImageField(null=True, blank=True,upload_to="volunteers/photos/")
+    photo = models.ImageField(null=True, blank=True, upload_to="volunteers/photos/")
     name = models.CharField(max_length=255)
     about = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
@@ -14,5 +13,3 @@ class Volunteer(BaseModel):
 
     def __str__(self):
         return self.name
-    class Meta:
-        unique_together = ['event', 'email']
