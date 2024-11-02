5370b3084d795ac693947b94b0dbbdf14caa14f5
DevilsAutumn <bhuvnesh875@gmail.com>
2024-11-02 15:22:40 +0530

    added new fields in EventAttendeeSerializer

--- backend/djangoindia/api/serializers/event.py
+++ backend/djangoindia/api/serializers/event.py
@@ -63,6 +63,8 @@ class EventAttendeeSerializer(serializers.Serializer):
     full_name = serializers.SerializerMethodField()
     professional_status = serializers.ChoiceField(choices=EventRegistration.ProfessionalStatus)
     organization = serializers.CharField(max_length=100,required=False, allow_blank=True)
+    description = serializers.CharField()
+    gender = serializers.ChoiceField(choices=EventRegistration.Gender)
     email = serializers.EmailField()
     linkedin = serializers.URLField()
     github = serializers.URLField(required=False, allow_blank=True)
