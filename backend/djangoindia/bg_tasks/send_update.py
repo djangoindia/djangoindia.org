e8854a28e5fa81b80d02cd2c1ad994c482627a9a
Prakkhar <prakkhar.n03@gmail.com>
2024-12-01 12:28:20 +0530

    modify unsubscribe facility

--- backend/djangoindia/bg_tasks/send_update.py
+++ backend/djangoindia/bg_tasks/send_update.py
@@ -7,19 +7,22 @@ from django.core.mail import send_mass_mail
 @shared_task
 def send_mass_update_email_task(update_id):
     from djangoindia.db.models.update import Update
-
+    from djangoindia.db.models.communication import Subscriber
     try:
         update = Update.objects.get(id=update_id)
     except Update.DoesNotExist:
-        raise ValueError("Update not found")
+        raise ValueError(f"Update with id {update_id} not found.")
 
     email_objs = []
     for subscriber in update.recipients.all():
+        if not subscriber.unsubscribe_token:
+            subscriber.generate_unsubscribe_token()  
+        unsubscribe_url = f"https://djangoindia.org/unsubscribe/{subscriber.unsubscribe_token}"
         email_tuple = (
             f"Django India: {update.get_formatted_type()}",
-            update.html_template,
+            f"{update.html_template}<br><br><a href='{unsubscribe_url}'>Click here to unsubscribe</a>", 
             settings.DEFAULT_FROM_EMAIL,
-            [subscriber.email],
+            [subscriber.email]  
         )
         email_objs.append(email_tuple)
     try:
