2ecde32ec2ecce15c4ffd39990914c34b9cdef78
ehemp <ehempfling@gmail.com>
2024-12-08 14:38:41 -0500

    Fixed #244 -- Created apiDataCard.ts, configured DataCard count numbers for fetching actual data - Subscribers, Contributors, Stars

--- frontend/src/components/DataCard/DataCard.tsx
+++ frontend/src/components/DataCard/DataCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { FaCity, FaGithub, FaUsers } from 'react-icons/fa';
 import { useInView } from 'react-intersection-observer';
