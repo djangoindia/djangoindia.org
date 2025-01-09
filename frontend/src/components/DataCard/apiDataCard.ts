6d062b24dea0f78b5e85a7ed45a50965433295b9
ehemp <ehempfling@gmail.com>
2025-01-09 14:02:48 -0500

    Added api URLs to .env and .env.example

--- frontend/src/components/DataCard/apiDataCard.ts
+++ frontend/src/components/DataCard/apiDataCard.ts
@@ -3,6 +3,6 @@ export const requestOptions = {
     options: {method: "GET", headers: {'X-GitHub-Api-Version': '2022-11-28'}},
 };
 export const url = {
-    stargazers_count: "https://api.github.com/repos/djangoindia/djangoindia.org",
-    contributors: "https://api.github.com/repos/djangoindia/djangoindia.org/contributors",
+    stargazers_count: process.env.STARGAZERS_COUNT,
+    contributors: process.env.CONTRIBUTORS,
 };
