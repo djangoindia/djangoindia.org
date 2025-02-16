dd512208ff38b12c419ef44e16295fb14ed12ba5
Anurag Yadav <yadavanurag1310@gmail.com>
2025-02-16 17:31:58 +0530

    fixed task issue

--- backend/djangoindia/celery.py
+++ backend/djangoindia/celery.py
@@ -4,8 +4,8 @@ import os
 
 from celery import Celery
 from celery.schedules import crontab
-from djangoindia.bg_tasks.dbbackup import nightly_db_backup
-n
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoindia.settings.local")
 
 app = Celery("djangoindia")
@@ -13,7 +13,7 @@ app.conf.timezone = 'Asia/Kolkata'
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.conf.beat_schedule = {
     'nightly-db-backup': {
-        'task': 'nightly_db_backup',  
+        'task': 'djangoindia.bg_tasks.dbbackup.nightly_db_backup',  
         'schedule': crontab(hour=3, minute=0),  # triggers at 3:00 AM IST
     },
 }
