ca8d0579b9cb6cebbe68d11197b8c21f19192648
Mohsen <mka342@gmail.com>
2025-03-14 11:15:58 +0330

    add-HideUnhide

--- frontend/src/containers/Signup/Signup.tsx
+++ frontend/src/containers/Signup/Signup.tsx
@@ -93,16 +93,16 @@ const SignupForm = () => {
   };
 
   return (
-    <section className='relative flex min-h-screen w-full overflow-auto'>
+    <section className='relative flex size-full overflow-hidden'>
       <Link
         href='/home'
-        className='group pointer-events-auto fixed top-4 left-4 z-50 w-fit rounded-full p-3 transition-all duration-300 hover:bg-blue-100 hover:shadow-xl'
+        className='group pointer-events-auto absolute right-4 top-4 z-50 w-fit rounded-full p-3 transition-all duration-300 hover:bg-blue-100 hover:shadow-xl sm:left-4'
       >
         <FaHome className='text-2xl text-[#06038D] transition-transform duration-300 group-hover:scale-110' />
       </Link>
       <div className='z-10 flex flex-1 items-center justify-center'>
         <motion.div
-          className='w-full max-w-md px-6 sm:w-3/5 min-h-full pt-12'
+          className='w-4/5 sm:w-3/5'
           initial={{ x: -100 }}
           animate={{ x: 0 }}
           transition={{
@@ -117,7 +117,7 @@ const SignupForm = () => {
               Login here!
             </Link>
           </div>
-          <h3 className='text-5xl font-black text-[#06038D]'>
+          <h3 className='text-4xl font-black text-[#06038D]'>
             Create an account
           </h3>
           <span className='my-4 inline-block font-semibold text-gray-600'>
@@ -276,7 +276,7 @@ const SignupForm = () => {
         </motion.div>
       </div>
       <motion.div
-        className='fixed bottom-10 right-10 z-0 w-[800px] sm:right-1/3'
+        className='absolute bottom-1/3 right-1/4 z-0 size-[1400px] sm:right-1/2'
         initial={{ x: -100, y: -100 }}
         animate={{ x: -20, y: 0 }}
         transition={{
