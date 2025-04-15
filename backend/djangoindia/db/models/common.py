2df67f63053636efda62f623835def0803cc6ee8
Suraj Pisal <surajpisal113@gmail.com>
2025-04-15 21:01:03 +0530

    add model changes

--- backend/djangoindia/db/models/common.py
+++ backend/djangoindia/db/models/common.py
@@ -7,13 +7,16 @@ class BackgroundTaskLog(BaseModel):
         model to hold the information about the background task that runs.
     """
     class StatusChoices(models.TextChoices):
-        SUCCESSFULL = "successfully"
-        FAILURE = "FAILURE"
+        SUCCESSFULL = "successful"
+        FAILURE = "failure"
+        PENDING = "pending"
+        STARTED = "started"
+        
 
     name = models.CharField(max_length=256, null=False, blank=False)
     start_datetime = models.DateTimeField(null=False)
     end_datetime = models.DateTimeField(null=True, blank=True)
     args = models.JSONField(default=dict, null=True, blank=True, help_text="default arguments present in task")
     kwargs = models.JSONField(default=dict, null=True, blank=True, help_text="Additional arguments")
-    status = models.CharField(max_length=32, null=False, blank=False, choices=StatusChoices.choices)
+    status = models.CharField(max_length=32, default=StatusChoices.PENDING, null=False, blank=False, choices=StatusChoices.choices)
     log = models.CharField(max_length=512, null=True, blank=True)
